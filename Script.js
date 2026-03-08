b2d39cc564831aa889af467ebac602086d3706d3
metlha labane <bida25-250@thuto.bac.ac.bw>
2026-03-08 15:23:00 +0200

    added back to top button

--- Script.js
+++ Script.js
@@ -46,3 +46,29 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const backToTopButton = document.getElementById('back-to-top'); 
+
+ 
+
+window.addEventListener('scroll', function() { 
+
+    if (window.scrollY > 100) { 
+
+        backToTopButton.style.display = 'block'; 
+
+    } else { 
+
+        backToTopButton.style.display = 'none'; 
+
+    } 
+
+}); 
+
+ 
+
+backToTopButton.addEventListener('click', function() { 
+
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+
+}); 
